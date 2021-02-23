44678813c9174ea1eadcc111ed38cfa3eb9a8a02
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-02-23 11:40:47 -0300

    feat: timeout funcionality

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,20 +1,47 @@
+import { useState, useEffect } from "react";
 import { CountdownButton, CountdownContainer } from "../styles/components/CountdownStyle";
 
 function Countdown() {
+
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  const startCountdown = () => {
+      setActive(true);
+  }
+
+  useEffect(() => {
+    if(active && time > 0) {
+        setTimeout(() => {
+            setTime(time -1);
+        }, 1000)
+    }
+  }, [active, time])
+
   return (
     <div>
       <CountdownContainer>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </CountdownContainer>
-      <CountdownButton type="button">Start cicle</CountdownButton>
+      <CountdownButton
+       type="button"
+       onClick={startCountdown}
+      >Start cicle</CountdownButton>
     </div>
   );
 }
