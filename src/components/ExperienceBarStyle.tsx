6dd29ccded2fbe389fbc8a98e052c702dc57fdf9
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-02-23 09:57:30 -0300

    chore: intial structure with nextjs

---
+++ src/components/ExperienceBarStyle.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const HeaderExperience = styled.header`
+  display: flex;
+  align-items: center;
+
+  span {
+    font-size: 1.5rem;
+  }
+`;
+
+export const CurrentExperience = styled.span`
+    position: absolute;
+    top: 1.2rem;
+    transform: translateX(-50%);
+`
+
+export const ContainerExperience = styled.div`
+    flex: 1;
+    height: 0.4rem;
+    border-radius: 0.4rem;
+    margin: 0 2rem;
+    position: relative;
+    background: var(--gray-line);
+
+     div {
+    height: 0.4rem;
+    border-radius: 0.4rem;
+    background: var(--green);
+  }
+`
